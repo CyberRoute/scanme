0ad6ea003a30ae95d278b17adb0af8319c987394
CyberRoute <alessandro.bresciani2016@gmail.com>
2024-01-15 14:16:06 +0100

    docs

--- scanme/doc.go
+++ scanme/doc.go
@@ -1,2 +1,3 @@
-// Scanme is a Go package for network scanning using the GoPacket library. It allows scanning a single IP address for open ports using SYN scans.
+// Scanme is a Go package for network scanning using the GoPacket library. 
+//It allows scanning a single IP address for open ports using SYN scans.
 package scanme
